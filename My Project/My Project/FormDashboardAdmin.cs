fa1dfca1eb6a9ad1a5270d0a4dde944d4fa55efd
Chahad08 <75300398+Chahad08@users.noreply.github.com>
2021-09-23 12:58:03 +0600

    C# Project First Commit

---
+++ My Project/My Project/FormDashboardAdmin.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace My_Project
+{
+    public partial class FormDashboardAdmin : Form
+    {
+        private FormLogin FormLogin1 { get; set; }
+        public FormDashboardAdmin()
+        {
+            InitializeComponent();
+        }
+
+
+        public FormDashboardAdmin(FormLogin formLogin1, string info) : this()
+        {
+            this.FormLogin1 = formLogin1;
+            this.lblNameInfo.Text = info;
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            MessageBox.Show("Logout Successful");
+            FormLogin fl = new FormLogin();
+            fl.Show();
+        }
+
+        private void FormCategoryAdmin_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            MessageBox.Show("Do you want to Close this App?");
+            Application.Exit();
+        }
+
+        private void btnInvoice_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            string s3;
+            FormInvoice inv = new FormInvoice(this, s3 = "Admin");
+            inv.Show();
+        }
+
+        private void btnIPD_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            string s4, sa;
+            FormAdmission adm = new FormAdmission(this, s4 = "Admin", sa = "");
+            adm.Show();
+        }
+
+        private void btnEmpManagement_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            FormEmployee_Management emp = new FormEmployee_Management(this);
+            emp.Show();
+        }
+
+        private void btnExamAdd_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            string s5, sa, ss;
+            FormAddExam ae = new FormAddExam(this, s5 = "Admin", sa = "", ss = "");
+            ae.Show();
+        }
+
+        private void btnMatManage_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            string s6, sa, ss, sd;
+            FormMaterial_Management fma = new FormMaterial_Management(this, s6 = "Admin", sa = "", ss = "", sd = "");
+            fma.Show();
+        }
+
+        private void btnMedicine_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            string s6, sa, ss, sd, sf;
+            FormMedicineInfo fmi = new FormMedicineInfo(this, s6 = "Admin", sa = "", ss = "", sd = "", sf = "");
+            fmi.Show();
+        }
+    }
+}
---
+++ My Project/My Project/FormLogin.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace My_Project
+{
+    public partial class FormLogin : Form
+    {
+        private DataAccess Da { get; set; }
+        public FormLogin()
+        {
+            InitializeComponent();
+            this.Da = new DataAccess();
+        }
+
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                string sql = "select * from EmployeeInfo where EmpID = '" + this.txtUserId.Text.ToLower() + "' and Password = '" + this.txtPassword.Text + "';";
+                DataSet ds = Da.ExecuteQuery(sql);
+
+
+                if (ds.Tables[0].Rows.Count == 1)
+                {
+                    this.ClearContent();
+                    this.Hide();
+                    MessageBox.Show("Login Valid");
+                    string name = ds.Tables[0].Rows[0][1].ToString();
+                    if (ds.Tables[0].Rows[0][4].ToString() == "Admin")
+                    {
+                        FormDashboardAdmin da = new FormDashboardAdmin(this, name);
+                        da.Show();
+                    }
+                    else if (ds.Tables[0].Rows[0][4].ToString() == "Manager")
+                    {
+                        FormDashboardManager dm = new FormDashboardManager(this, name);
+                        dm.Show();
+                    }
+                    else if (ds.Tables[0].Rows[0][4].ToString() == "Receptionist")
+                    {
+                        FormDashboardReceptionist dr = new FormDashboardReceptionist(this, name);
+                        dr.Show();
+                    }
+                    else if (ds.Tables[0].Rows[0][4].ToString() == "Pharmacist")
+                    {
+                        FormDashBoardPharmacist dp = new FormDashBoardPharmacist(this, name);
+                        dp.Show();
+                    }
+
+
+                }
+                else
+                {
+                    MessageBox.Show("Login Invalid!");
+                    this.ClearContent();
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("An error has occured: " + exc.Message);
+            }
+     }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ClearContent()
+        {
+            this.txtUserId.Clear();
+            this.txtPassword.Clear();
+        }
+
+        private void FormLogin_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            MessageBox.Show("Do you want to Close this App?");
+            Application.Exit();
+        }
+    }
+}
